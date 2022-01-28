6922d369e1a0933a9012e8e9ea09c6bfe7c366a3
Shunji Nakashima <3724063+nakashimmer@users.noreply.github.com>
2022-01-28 13:46:22 +0900

    up

---
+++ bmi/service-worker copy.js
@@ -0,0 +1,11 @@
+//PWAのserviceworker.jsをコーディング
+// Language: javascript	
+// Path: bmi/service-worker.js
+// Compare this snippet from bmi/service-worker.js:
+// const cacheName = "bmi1";
+// const urlsToCache = [
+// 	"./apple-touch-icon.png",
+// 	"./js.js",
+// 	"./html5-192.png",
+// 	"./html5-256.png",
+// 	"./index.html",
